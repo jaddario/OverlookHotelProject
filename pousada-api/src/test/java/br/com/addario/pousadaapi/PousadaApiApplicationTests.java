50f5dcb82e74a4599a9dc5837336d26ea906268d
José Addario <j.addario@gmail.com>
2020-10-14 10:24:55 -0300

    Commit inicial do projeto

---
+++ pousada-api/src/test/java/br/com/addario/pousadaapi/PousadaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.addario.pousadaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PousadaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
